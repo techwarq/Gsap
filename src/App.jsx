e82a40739f814a5cdbc4a4161524f5662950f58d
techwarq <102250604+techwarq@users.noreply.github.com>
2024-08-04 21:58:37 +0530

    svv roadmap

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+const Roadmap = () => {
+  return (
+    <div className="w-full h-screen bg-gray-900 flex flex-col items-center justify-center">
+  <svg viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg">
+  <defs>
+              <marker
+                id="arrow"
+                markerWidth="3"
+                markerHeight="3"
+                refX="1.5"
+                refY="1.5"
+                orient="auto"
+              >
+                <path d="M 0 0 L 3 1.5 L 0 3 z" fill="white" />
+              </marker>
+              <marker
+                id="arrow2"
+                markerWidth="3"
+                markerHeight="3"
+                refX="2.5"
+                refY="2.5"
+                orient="auto"
+              >
+                <path d="M 0 -5 L 10 0 L 0 5" fill="white" />
+              </marker>
+            </defs>
+  <path strokeWidth="2.5" fill="transparent" markerEnd="url(#arrow)"  stroke="white" d="M 232.207 51.126 C 232.207 59.732 233.571 85.92 239.903 93.461 C 247.166 102.112 315.222 94.283 326.189 96.627 C 348.039 101.296 337.057 127.284 337.523 146.739"></path>
+  <path stroke="white" fill="transparent" markerEnd="url(#arrow)"  strokeWidth="2.5" d="M 136.494 147.208 C 136.494 138.602 134.738 112.329 140.662 104.788 C 147.457 96.137 211.122 103.966 221.382 101.622 C 241.823 96.953 231.549 70.965 231.985 51.51"></path>
+  <rect x="191.323" y="13.118" width="95.781" height="41.975" fill="blue" ></rect>
+  <path stroke="white" fill="transparent" markerEnd="url(#arrow2)"  strokeWidth="2.5" d="M 28.803 281.042 C 28.803 272.436 26.498 244.84 32.422 237.299 C 39.217 228.648 111.795 236.477 122.055 234.133 C 142.496 229.464 132.222 203.476 132.658 184.021"></path>
+  <path strokeWidth="2.5" fill="transparent" markerEnd="url(#arrow)"  stroke="white" d="M 132.274 181.99 C 132.274 190.596 137.546 214.781 139.97 224.325 C 145.028 244.236 223.928 235.438 235.004 237.227 C 257.061 240.79 247.475 269.657 247.941 289.112"></path>
+  <rect x="296.628"  y="150.4" width="95.781" height="41.975" fill="blue" ></rect>
+  <rect x="91.554"   y="150.551"  width="95.781" height="41.975" fill="blue" ></rect>
+  <path strokeWidth="2.5" fill="transparent" markerEnd="url(#arrow)"  stroke="white" d="M 235.5 327.061 C 235.5 335.667 236.864 361.855 243.196 369.396 C 250.459 378.047 318.515 370.218 329.482 372.562 C 351.332 377.231 340.35 403.219 340.816 422.674"></path>
+  <path stroke="white" fill="transparent" markerEnd="url(#arrow)"  strokeWidth="2.5" d="M 138.757 420.431 C 138.757 411.825 137.001 385.552 142.925 378.011 C 149.72 369.36 213.385 377.189 223.645 374.845 C 244.086 370.176 233.812 344.188 234.248 324.733"></path>
+  <rect x="200.515"  y="294.143"  width="95.781" height="41.975" fill="blue" ></rect>
+  <rect x="290.176"  y="427.956"  width="95.781" height="41.975" fill="blue" ></rect>
+  <rect x="90.102"  y="420.613"  width="95.781" height="41.975" fill="blue" ></rect>
+  <path strokeWidth="2.5" markerEnd="url(#arrow)" fill="transparent" stroke="white" d="M 350.527 190.179 C 350.527 198.785 355.799 222.97 358.223 232.514 C 363.281 252.425 442.181 243.627 453.257 245.416 C 475.314 248.979 465.728 277.846 466.194 297.301"></path>
+  <rect x="397.458"  y="301.727"  width="95.781" height="41.975" fill="blue" ></rect>
+  <rect x="1.713"  y="284.477"  width="95.781" height="41.975" fill="blue" ></rect>
+  <text fill="white" fontFamily="Arial, sans-serif" fontSize="14" x='16.88' y='310.176'>Linked Lists</text>
+  <text fill="white" fontFamily="Arial, sans-serif" fontSize="14" x='225.122' y='315.268'>array</text>
+</svg>
+    </div>
+  );
+};
+
+export default Roadmap;
